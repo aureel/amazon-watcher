56bd88a57cb07632e425e2b39399486b99638745
Aurelien Vitse <avitse@gmail.com>
2021-04-24 21:04:20 -0400

    new products

--- src/cron-config.ts
+++ src/cron-config.ts
@@ -1,7 +1,9 @@
 const cronConfig = [
-  { cronRule: "*/20 * * * * *", amazonProductId: "B08L8LG4M3", priceThreshold: 950 },
-  { cronRule: "* * * * *", amazonProductId: "B08LF1CWT2", priceThreshold: 2100 },
-  { cronRule: "0 20 * * *", amazonProductId: "B08L8LG4M3" },
+  { cronRule: "*/20 * * * * *", amazonProductId: "B08L8LG4M3", priceThreshold: 900 },
+  { cronRule: "*/20 * * * * *", amazonProductId: "B08LF1CWT2", priceThreshold: 900 },
+  { cronRule: "*/20 * * * * *", amazonProductId: "B08P3V572B", priceThreshold: 900 },
+  { cronRule: "*/20 * * * * *", amazonProductId: "B08KY266MG", priceThreshold: 900 },
+  // { cronRule: "* * * * *", amazonProductId: "B08LF1CWT2", priceThreshold: 2100 },
 ];
 
 export { cronConfig };
